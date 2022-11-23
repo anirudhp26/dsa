a0017a7fdd4279d32c0de30b417acc6747f67c8b
Anirudh Patel <anirudh26p@gmail.com>
2022-11-23 14:38:54 +0530

    customstack with variable size implemented

--- Main.java
+++ Main.java
@@ -2,18 +2,31 @@ package dsa.dsa;
 
 public class Main {
     public static void main(String[] args) {
-        linkedlists ll = new linkedlists();
+        customStacks stack = new customStacks(5);
 
-        ll.add(5,0);
-        ll.display();
-        ll.add(4,1);
-        ll.display();
-        ll.add(25,1);
-        ll.display();
-        ll.add(2,2);
-        // ll.add(54,0);
-        ll.display();
-        ll.insertRec(10, 2);
-        ll.display();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(67);
+        stack.push(5487);
+        stack.display();
+
+        // linkedlists ll = new linkedlists();
+        // ll.add(5,0);
+        // ll.display();
+        // ll.add(4,1);
+        // ll.display();
+        // ll.add(25,1);
+        // ll.display();
+        // ll.add(2,2);
+        // // ll.add(54,0);
+        // ll.display();
+        // ll.insertRec(10, 2);
+        // ll.display();
+
+        
     }
 }
--- stacks.java
+++ stacks.java
@@ -1,5 +1,7 @@
 package dsa.dsa;
 
+import java.util.Arrays;
+
 class customStacks{
     private int[] data;
     int ptr = -1;
@@ -12,15 +14,52 @@ class customStacks{
         this.data = new int[size];
     }
 
-    public boolean push(int item){
+    public void push(int item){
+        if (ptr == data.length - 1) {
+            data = Arrays.copyOf(data, ptr*2);
+        }
         ptr++;
         data[ptr] = item;
+    }
+    public boolean pop(){
+        if (isEmpty()) {
+            System.out.println("Cannot Remove an element from an Empty Stack");
+            return false;
+        }
+        ptr--;
         return true;
     }
-    public int pop(){
-        int removedEle = data[ptr];
-        data[ptr] = null;
-        ptr--;
-        return removedEle;
+
+    public void display(){
+        // System.out.println(Arrays.toString(data));
+        for (int i = 0; i <= ptr; i++) {
+            System.out.print(data[i]);
+            System.out.print("-->");
+        }
+        System.out.println("Top of Stack");
+    }
+
+    public boolean search(int item) {
+        if (isEmpty()) {
+            System.out.println("Cannot Search elements from an Empty Stack");
+            return false;
+        }
+        else {
+            for (int i = 0; i <= ptr; i++) {
+                if (data[i] == item) {
+                    System.out.print("DATA FOUND AT INDEX: ");
+                    System.out.print(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public boolean isEmpty(){
+        if (ptr == -1) {
+            return true;
+        }
+        return false;
     }
 }
